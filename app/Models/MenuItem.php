c658d867791d953996ef741e5f23d2b196164c05
Ilya Devin <i.devin@creotech.org>
2022-06-06 20:47:52 +0400

    menu item model sample

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    public $timestamps = true;
 
+    protected $table = 'menu_items';
+
+    protected $fillable = [
+        'name', 'name', 'url', 'parent_id'
+    ];
 }
